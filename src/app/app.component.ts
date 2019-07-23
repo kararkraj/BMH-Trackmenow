69459c97f5370f434de43bfbbcea040eacec7e49
Karthik Nagaraj <ankarthikengg@gmail.com>
2019-07-23 15:05:42 +0530

    side menu integration complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Router } from '@angular/router';
+import { Router, RouterEvent } from '@angular/router';
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
@@ -11,6 +11,59 @@ import { AuthService } from './public/services/auth/auth.service';
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+
+  public appPages = [
+    {
+      title: 'MY PROFILE',
+      url: 'protected/tabs/vehicle-list',
+      icon: 'person',
+      detail: false,
+      detailIcon: ""
+    },
+    {
+      title: 'REPORTS',
+      url: '',
+      icon: 'paper',
+      detail: true,
+      detailIcon: "arrow-dropdown",
+      hideSubPages: true,
+      subPages: [
+        {
+          title: 'SPEED',
+          url: 'protected/tabs/vehicle-list',
+        },
+        {
+          title: 'FUEL',
+          url: 'protected/tabs/vehicle-list',
+        },
+        {
+          title: 'TRIP',
+          url: 'protected/tabs/vehicle-list',
+        },
+        {
+          title: 'DISTANCE/KM',
+          url: 'protected/tabs/vehicle-list',
+        },                        
+      ]
+    },
+    {
+      title: 'CONTACT US',
+      url: 'protected/tabs/vehicle-list',
+      icon: 'call',
+      detail: false,
+      detailIcon: ""
+    },
+    {
+      title: 'LOGOUT',
+      url: 'protected/tabs/vehicle-list',
+      icon: 'log-out',
+      detail: false,
+      detailIcon: ""
+    }
+  ];
+
+  private activatedMenuPath:string;
+
   constructor(
     private platform: Platform,
     private statusBar: StatusBar,
@@ -18,6 +71,9 @@ export class AppComponent {
     private router: Router
   ) {
     this.initializeApp();
+    this.router.events.subscribe((event: RouterEvent) => {
+      this.activatedMenuPath = event.url;
+    });
   }
 
   initializeApp() {
@@ -25,7 +81,7 @@ export class AppComponent {
       this.statusBar.styleLightContent();
       this.statusBar.overlaysWebView(false);
       this.statusBar.backgroundColorByHexString('#184F80');
-       this.auth.authenticationState.subscribe(state => {
+      this.auth.authenticationState.subscribe(state => {
         if (state) {
           this.router.navigate(['protected', 'tabs']);
         } else {
@@ -33,5 +89,9 @@ export class AppComponent {
         }
       });
     });
+  }
+
+  toggleSubPages() {
+    this.appPages[1].hideSubPages = !this.appPages[1].hideSubPages;
   }
 }
