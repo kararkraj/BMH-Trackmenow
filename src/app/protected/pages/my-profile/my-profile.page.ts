64f065f349eeb46b702f3ebb55859bcc419fdeb6
Karthik Nagaraj <ankarthikengg@gmail.com>
2019-07-28 21:28:13 +0530

    created interface user and my-profile view

--- src/app/protected/pages/my-profile/my-profile.page.ts
+++ src/app/protected/pages/my-profile/my-profile.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
 import { UserService } from './../../services/user/user.service';
+import { User } from './../../services/user/user';
 
 @Component({
   selector: 'app-my-profile',
@@ -8,14 +11,35 @@ import { UserService } from './../../services/user/user.service';
 })
 export class MyProfilePage implements OnInit {
 
+  private userForm = new FormGroup({
+    "Firstname": new FormControl(''),
+    "LastName": new FormControl(''),
+    "UserName": new FormControl(''),
+    "Email": new FormControl(''),
+    "MobileNo": new FormControl(0),
+    "Address": new FormControl(''),
+    "CompanyName": new FormControl(''),
+    "TotalVehicles": new FormControl(0)
+  });
+
   constructor(
     private userService: UserService
   ) { }
 
   ngOnInit() {
     this.userService.setHeaders().then(() => {
-      this.userService.getUserDetails().subscribe((user) => {
+      this.userService.getUserDetails().subscribe((user: User[]) => {
         console.log(user);
+        this.userForm.patchValue({
+          Firstname: user[0]["Firstname"],
+          LastName: user[0]["LastName"],
+          UserName: user[0]["UserName"],
+          Email: user[0]["Email"],
+          MobileNo: user[0]["MobileNo"],
+          Address: user[0]["Address"],
+          CompanyName: user[0]["CompanyName"],
+          TotalVehicles: user[0]["TotalVehicles"]
+        });
       });
     });
   }
---
+++ src/app/protected/services/user/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+  "Firstname": string,
+  "LastName": string,
+  "UserName": string,
+  "Email": string,
+  "MobileNo": number,
+  "Address": string,
+  "Pincode": number,
+  "CompanyName": string,
+  "TotalVehicles": number
+}
