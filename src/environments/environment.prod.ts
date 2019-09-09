2c25b220259ad2ca24dec96529de98038188956e
gayathrithanujaalluri@gmail.com <gayathrithanujaalluri@gmail.com>
2019-09-09 16:54:33 +0530

    updated production environment and add tractor vehicles

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,13 +7,22 @@ export const environment = {
     login: "Account/Login",
     getVehicles: "Vehicle/GetAll",
     getUserProfile: "UserProfile/Get",
+    updateUserProfile: "UserProfile/Update",
     getVehicleTypes: "VehicleType/Get",
     getGPSDevices: "Device/Get",
-    addVehicle: "Vehicle/Create"
+    addVehicle: "Vehicle/Create",
+    speedReport: "SpeedReport/Get",
+    distanceReport: "DistanceReport/Get",
+    fuelReport: "FuelReport/Get",
+    contactUs: "ContactUs/SendMessage"
   },
   messages: {
     credentialsMismatch: "Please enter valid credentials.",
     noInternet: "Please make sure you have internet connectivity",
-    somethingWrong: "Something went wrong. Please try again after some time."
+    somethingWrong: "Something went wrong. Please try again after some time.",
+    reportDataUnavailable: "The noun data of the vehicle is unavailable in the selected date range.",
+    noDevices: "There are no new devices to add.",
+    userProfileUpdated: "Thanks for updating your profile",
+    contactUs: "Your message is sent successfully."
   }
 };
